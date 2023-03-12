d80e73d92265870ea846e968ed679c8bd9947919
Aleksandr Belyaev <belas80@gmail.com>
2023-03-12 22:36:15 +0500

    09.05 changed network address

--- 09-ci-05-teamcity/terraform/network.tf
+++ 09-ci-05-teamcity/terraform/network.tf
@@ -5,7 +5,7 @@ resource "yandex_vpc_network" "lab-net" {
 
 # Subnet b
 resource "yandex_vpc_subnet" "subnet-b" {
-  v4_cidr_blocks = ["192.168.11.0/24"]
+  v4_cidr_blocks = ["192.168.1.0/24"]
   zone           = "ru-central1-b"
   name           = "subnet-b"
   network_id     = yandex_vpc_network.lab-net.id
